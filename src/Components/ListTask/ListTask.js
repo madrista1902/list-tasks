1bf82afa39157f05c120d558e34ded2ed0d41b07
madrista1902 <124149904+madrista1902@users.noreply.github.com>
2023-05-14 15:40:29 +0200

    Add files via upload

--- src/Components/ListTask/ListTask.js
+++ src/Components/ListTask/ListTask.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addTask, toggleTaskStatus, deleteTask } from '../../JS/Actions';
 import AddTask from '../AddTask/AddTask';
+import FilterTask from '../FilterTask/FilterTask';
 
 const ListTask = () => {
   const tasks = useSelector((state) => state.tasks);
+  const [filter, setFilter] = useState('all');
+
+  const handleFilterChange = (newFilter) => {
+    setFilter(newFilter);
+  };
+
   const dispatch = useDispatch();
 
   const handleAddTask = (newTask) => {
@@ -19,11 +26,23 @@ const ListTask = () => {
     dispatch(deleteTask(taskId));
   };
 
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === 'all') {
+      return true;
+    } else if (filter === 'done') {
+      return task.isDone;
+    } else if (filter === 'notDone') {
+      return !task.isDone;
+    }
+    return true;
+  });
+
   return (
     <div>
       <h2>Tasks</h2>
+      <FilterTask onFilterChange={handleFilterChange} />
       <ul>
-        {tasks.map((task) => (
+        {filteredTasks.map((task) => (
           <li key={task.id}>
             <input
               type="checkbox"
